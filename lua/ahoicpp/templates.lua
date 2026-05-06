e373a61c1d316b453d51dc77e9971d1e6984f5f2
martuscellifaria <martuscellifaria@gmail.com>
2026-05-06 18:59:27 +0200

    Added a few fixes on dev errors

--- lua/ahoicpp/templates.lua
+++ lua/ahoicpp/templates.lua
@@ -270,6 +270,7 @@ function M.get_version_h_in()
 #define PROJECT_VERSION_MAJOR @PROJECT_VERSION_MAJOR@
 #define PROJECT_VERSION_MINOR @PROJECT_VERSION_MINOR@
 #define PROJECT_VERSION_PATCH @PROJECT_VERSION_PATCH@
+]]
 end
 
 function M.get_version_rc_in()
